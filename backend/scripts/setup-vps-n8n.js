98648ce58515ce6b38cb19faa9717539b0a5b7db
josemanuel19992021 <josemanuel19992021@gmail.com>
2026-07-08 07:23:09 -0400

    [TECH/INFRA] Remove trailing slash from N8N_SUBPATH environment variable

--- backend/scripts/setup-vps-n8n.js
+++ backend/scripts/setup-vps-n8n.js
@@ -61,7 +61,7 @@ conn.on('ready', async () => {
             -p 5678:5678 \\
             -e EXECUTIONS_PROCESS=main \\
             -e N8N_PORT=5678 \\
-            -e N8N_SUBPATH=/n8n/ \\
+            -e N8N_SUBPATH=/n8n \\
             -e WEBHOOK_URL=https://testing.jhoanes.com/n8n/ \\
             -v /root/.n8n:/home/node/.n8n \\
             n8nio/n8n:latest
